541972997f41a10ea77a5c17b00564e1087332e5
Trickey <lynntric@amazon.com>
2017-08-18 17:11:08 -0700

    translating to JAVA

--- restrictedArrayJava/src/CSFunPractice.java
+++ restrictedArrayJava/src/CSFunPractice.java
@@ -2,5 +2,14 @@ public class CSFunPractice {
 
     public static void main(String[] args) {
 
+
+        // create a new restricted array
+        RestrictedArray myRestrictedArray = new RestrictedArray();
+        System.out.print(myRestrictedArray.get(0) + "\n");
+        System.out.print(myRestrictedArray.length()  + "\n");
+        myRestrictedArray.print();
+
+        myRestrictedArray.reverse();
+        myRestrictedArray.print();
     }
 }
--- restrictedArrayJava/src/restrictedArray.java
+++ restrictedArrayJava/src/restrictedArray.java
@@ -6,9 +6,61 @@ public class RestrictedArray {
     private Random rand = new Random();
     private int size = rand.nextInt(20) + 1;
 
-    //constructor
+    //constructor for now only doing with random size and numbers
     public RestrictedArray() {
+        internalArray = new ArrayList();
+        for (int i = 0; i < size; i++)
+        {
+            Integer r = rand.nextInt() % 256;
+            this.internalArray.add(r);
+        }
     }
         
+    public java.lang.Object get(int key) {
+        if (key >= 0 && key < size) {
+            return this.internalArray.get(key);
+        } else return null;
+    }
+
+    public void set(int key, int value) {
+        if (key >= 0 && key < size) {
+            this.internalArray.set(key, value);
+        }
+    }
+
+    // measure length
+    public int length() {
+        int i = 0;
+        int count = 0;
+        while (this.get(i) != null) {
+            count ++;
+            i ++;
+        }
+        return count;
+    }
+
+    // print array
+    public void print() {
+        int i = 0;
+        while (this.get(i) != null) {
+            System.out.print(this.get(i) + " ");
+            i ++;
+        }
+        System.out.print("\n");
+    }
+
+    //reverse array
+    public void reverse() {
+        int i = 0;
+        int j = this.length() - 1;
+        while (i <= j) {
+            int numAtI = (int) this.get(i);
+            int numAtJ = (int) this.get(j);
+            this.set(i, numAtJ);
+            this.set(j, numAtI);
+            i ++;
+            j --;
+        }
+    }
 
 }
